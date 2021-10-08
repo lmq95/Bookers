546834202ad9ca6df96cf7ec0e9446f0ed70ac1a
lmq95 <4321namap@gmail.com>
2021-10-08 14:47:22 +0000

    [update]3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,19 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @list = Book.find(params[:id])
   end
 end
+
+
+private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
