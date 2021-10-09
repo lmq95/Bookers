7e24fdef49f61cf2bffd5eab941aa08d16824605
lmq95 <4321namap@gmail.com>
2021-10-09 16:57:08 +0000

    [update]3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,12 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id)
+    else
+    render :index
+    end  
   end
 
   def show
@@ -16,6 +19,12 @@ class BooksController < ApplicationController
 
   def edit
     @list = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    
   end
   
   def destroy
