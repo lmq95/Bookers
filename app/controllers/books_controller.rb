af108b311ae522de246d5dc47ea996480f523925
lmq95 <4321namap@gmail.com>
2021-10-09 06:15:46 +0000

    [mvc]3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,13 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def show
